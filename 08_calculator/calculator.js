9834576a1fdeae1e90032a511966ac8da87c36d9
jdogknigh <jdegraffe313@gmail.com>
2022-09-26 21:20:37 -0400

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let total = (a+b)
+  return total
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	let total = (a-b)
+  return total
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+
+for (const value of arr) {
+  sum += value;
+}
+return sum
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  var total = 1;
+  for (var i = 0; i < arr.length; i++) {
+      total = total * arr[i];
+  }
+  return total;
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let total = Math.pow(a,b)
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let total = 1
+  for (i=1; i<= x; i++)
+  {
+   total = total *i 
+  }
+  return total 
 };
-
 // Do not edit below this line
 module.exports = {
   add,
